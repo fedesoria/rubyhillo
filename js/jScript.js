e402049fa99b66fd7e88b90d06730251aef79c5f
Federico Soria <fedesoria@gmail.com>
2011-07-12 19:20:45 -0700

    first commit

---
+++ js/jScript.js
@@ -0,0 +1,24 @@
+	
+$(document).ready(function(){
+
+	
+
+	//CUFON
+	Cufon.replace('h1'); 
+	Cufon.replace('#slidecaption'); 
+	
+	currentTime = new Date()
+	//SET COUNTER DATE
+	$("#countdown_dashboard").countDown({
+		targetOffset: {
+			'day': 		(13 - currentTime.getDate()),
+			'month': 	0,
+			'year': 	0,
+			'hour': 	(18 - currentTime.getHours()),
+			'min': 		(60 - currentTime.getMinutes()),
+			'sec': 		0
+		},
+		omitWeeks: true
+	});
+	
+});
